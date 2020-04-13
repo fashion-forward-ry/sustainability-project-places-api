3d43fe789ef1fc38e393389653b78ab8d1da118e
annisall <anniina.sallinen@gmail.com>
2020-04-13 13:19:05 +0300

    Set query params as they should be set instead of just concatenating them to the string

--- src/sustainability_project_places_api/places-api.clj
+++ src/sustainability_project_places_api/places-api.clj
@@ -6,5 +6,7 @@
 ;; Example of calling the function:
 ;; (auth/get-place-detail "<key-to-api>" "<place-id>" "https://maps.googleapis.com/maps/api/place")
 (defn get-place-detail [key place-id base-url]
-  (let [resp (http/get (str base-url "/details/json?key=" key "&place_id=" place-id))]
+  (let [resp (http/get (str base-url "/details/json")
+                       {:query-params {"key" key
+                                       "place_id" place-id}})]
     (json/parse-string (:body resp) true)))
